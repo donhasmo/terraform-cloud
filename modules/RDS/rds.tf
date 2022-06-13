9ba1d660b508559708436e98062feb6b777b0f7f
donhasmo <alhassani.y3k@gmail.com>
2022-06-13 06:31:24 -0800

    update

--- modules/RDS/rds.tf
+++ modules/RDS/rds.tf
@@ -34,7 +34,23 @@ resource "aws_db_instance" "ACS-rds" {
   username               = var.db-username
   password               = var.db-password
   parameter_group_name   = "default.mysql5.7"
-  db_subnet_group_name   = aws_db_subnet_group.ACS-rds.name
+  db_subnet_group_name   = aws_db_subnet_group.ACS-rds1.name
+  skip_final_snapshot    = true
+  vpc_security_group_ids = var.db-sg
+  multi_az               = "true"
+}
+
+resource "aws_db_instance" "ACS-rds" {
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t2.micro"
+  name                   = "hassandb"
+  username               = var.db-username
+  password               = var.db-password
+  parameter_group_name   = "default.mysql5.7"
+  db_subnet_group_name   = aws_db_subnet_group.ACS-rds2.name
   skip_final_snapshot    = true
   vpc_security_group_ids = var.db-sg
   multi_az               = "true"
